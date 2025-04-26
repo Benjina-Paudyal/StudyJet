a73c155ea5df8856114fbe5c6d239ae305efcd84
Benjina Paudyal <benjina.paudyal@gmail.com>
2025-04-26 15:13:12 +0200

    Added UploadCVController

---
+++ StudyJet.API/Controllers/UploadCVController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudyJet.API.Services.Interface;
+
+namespace StudyJet.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadCVController : ControllerBase
+    {
+        private readonly IFileStorageService _fileStorageService;
+
+        public UploadCVController(IFileStorageService fileStorageService)
+        {
+            _fileStorageService = fileStorageService;
+        }
+
+        [HttpPost("upload-cv")]
+        public async Task<IActionResult> UploadCV(IFormFile cvFile)
+        {
+            if (cvFile == null)
+            {
+                return BadRequest("No file uploaded.");
+            }
+
+            try
+            {
+                // Save the CV file and get the URL
+                var fileUrl = await _fileStorageService.SaveCVAsync(cvFile);
+
+                // Return the file URL or any other response you need
+                return Ok(new { fileUrl });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
