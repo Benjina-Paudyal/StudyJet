f0ec519c59035560db4869d0ac89eb8043a17124
Benjina Paudyal <benjina.paudyal@gmail.com>
2025-04-26 13:39:28 +0200

    Added Mission COmponent

--- StudyJet.UI/src/app/homepage/homepage.component.ts
+++ StudyJet.UI/src/app/homepage/homepage.component.ts
@@ -3,11 +3,17 @@ import { ShowcaseComponent } from '../showcase/showcase.component';
 import { PopularCourseComponent } from "../popular-course/popular-course.component";
 import { TestimonialComponent } from '../testimonial/testimonial.component';
 import { ReactPromoComponent } from '../react-promo/react-promo.component';
+import { MissionComponent } from '../mission/mission.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [ShowcaseComponent, PopularCourseComponent, TestimonialComponent, ReactPromoComponent],
+  imports: [
+    ShowcaseComponent, 
+    PopularCourseComponent, 
+    TestimonialComponent, 
+    ReactPromoComponent,
+    MissionComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
